c7b8ba3c3426bf2a039c613cc37b8ddd9fd0570e
mohamadomari <mohammadalomari353535@gmail.com>
2024-03-16 18:12:04 +0300

    first push

---
+++ src/test/java/fintech/omari/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fintech.omari.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
